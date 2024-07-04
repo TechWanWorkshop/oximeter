e96a2ac346621c99965267656e1afea8973809de
Tech Wan Workshop <59522582+TechWanWorkshop@users.noreply.github.com>
2024-07-04 14:09:50 +0700

    Add files via upload

---
+++ oxi.ino
@@ -0,0 +1,137 @@
+#include <Wire.h>
+#include "MAX30105.h" //sparkfun MAX3010X library
+#include "heartRate.h"
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);  
+
+MAX30105 particleSensor;
+
+
+double avered = 0; double aveir = 0;
+double sumirrms = 0;
+double sumredrms = 0;
+int i = 0;
+int Num = 200;//calculate SpO2 by this sampling interval
+
+double ESpO2 = 95.0;//initial value of estimated SpO2
+double FSpO2 = 0.7; //filter factor for estimated SpO2
+double frate = 0.95; //low pass filter for IR/red LED value to eliminate AC component
+#define TIMETOBOOT 3000 // wait for this time(msec) to output SpO2
+#define SCALE 88.0 //adjust to display heart beat and SpO2 in the same scale
+#define SAMPLING 5 //if you want to see heart beat more precisely , set SAMPLING to 1
+#define FINGER_ON 30000 // if red signal is lower than this , it indicates your finger is not on the sensor
+#define MINIMUM_SPO2 80.0
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
+
+
+#define USEFIFO
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
+  lcd.init();                 
+  lcd.backlight();
+
+
+  
+  
+  // Initialize sensor
+  while (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30102 was not found. Please check wiring/power/solder jumper at MH-ET LIVE MAX30102 board. ");
+    //while (1);
+  }
+
+  //Setup to sense a nice looking saw tooth on the plotter
+  byte ledBrightness = 0x7F; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  //Options: 1 = IR only, 2 = Red + IR on MH-ET LIVE MAX30102 board
+  int sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  // Set up the wanted parameters
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+  particleSensor.enableDIETEMPRDY();
+
+}
+
+void loop()
+{
+
+  lcd.setCursor(0, 0);     //
+  lcd.print("   Pulse Oximeter");
+
+  
+  uint32_t ir, red , green;
+  double fred, fir;
+  double SpO2 = 0; //raw SpO2 before low pass filtered
+
+#ifdef USEFIFO
+  particleSensor.check(); //Check the sensor, read up to 3 samples
+
+  while (particleSensor.available()) {//do we have new data
+#ifdef MAX30105
+   red = particleSensor.getFIFORed(); //Sparkfun's MAX30105
+    ir = particleSensor.getFIFOIR();  //Sparkfun's MAX30105
+#else
+    red = particleSensor.getFIFOIR(); //why getFOFOIR output Red data by MAX30102 on MH-ET LIVE breakout board
+    ir = particleSensor.getFIFORed(); //why getFIFORed output IR data by MAX30102 on MH-ET LIVE breakout board
+#endif
+
+    
+   
+    
+    i++;
+    fred = (double)red;
+    fir = (double)ir;
+    avered = avered * frate + (double)red * (1.0 - frate);//average red level by low pass filter
+    aveir = aveir * frate + (double)ir * (1.0 - frate); //average IR level by low pass filter
+    sumredrms += (fred - avered) * (fred - avered); //square sum of alternate component of red level
+    sumirrms += (fir - aveir) * (fir - aveir);//square sum of alternate component of IR level
+    if ((i % SAMPLING) == 0) {//slow down graph plotting speed for arduino Serial plotter by thin out
+      if ( millis() > TIMETOBOOT) {
+        
+        if (ir < FINGER_ON) ESpO2 = MINIMUM_SPO2; //indicator for finger detached
+        float temperature = particleSensor.readTemperatureF();
+
+        //lcd.setCursor(17, 1);     //(col, row)
+       // lcd.print("         ");
+        
+        Serial.print(" Oxygen % = ");
+        Serial.println(ESpO2);
+
+        lcd.setCursor(0, 2);     //(col, row)
+        lcd.print("Blood Oxygen %: ");
+        lcd.print(ESpO2,1);
+
+        
+      }
+    }
+    if ((i % Num) == 0) {
+      double R = (sqrt(sumredrms) / avered) / (sqrt(sumirrms) / aveir);
+      // Serial.println(R);
+      SpO2 = -23.3 * (R - 0.4) + 100; 
+      ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2;//low pass filter
+      //  Serial.print(SpO2);Serial.print(",");Serial.println(ESpO2);
+      sumredrms = 0.0; sumirrms = 0.0; i = 0;
+      break;
+    }
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    //Serial.println(SpO2);
+    
+  }
+
+  
+#endif
+}
